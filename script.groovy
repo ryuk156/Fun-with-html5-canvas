a03a7c890b34ec3e15ae1176c9d255a1cf2f4e6d
ryuk156 <yp15601560@gmail.com>
2021-03-28 16:30:29 +0530

    changed groovy

--- script.groovy
+++ script.groovy
@@ -8,7 +8,7 @@ def fetch() {
 
 	println("hello")
 	def repos = []
-	def org = 'ryuk210';
+	def org = 'terasology';
 	def githubCom = GitHub.connectUsingOAuth('ddae1e41099bb89a636241818107dff969c27695');
 
 	githubCom.getOrganization(org).listRepositories().each {
