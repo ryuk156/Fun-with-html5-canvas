560cbb68763734fe75ebf063dec281cdcde8ce9b
ryuk156 <yp15601560@gmail.com>
2021-03-28 15:49:48 +0530

    changed groovy

--- script.groovy
+++ script.groovy
@@ -5,14 +5,16 @@ package org.kohsuke.github
 import org.kohsuke.github.GitHub
 
 def fetch() {
-	def repos = []
-	def org = 'ryuk156';
-	def githubCom = GitHub.connectUsingOAuth('ddae1e41099bb89a636241818107dff969c27695');
 
-	githubCom.getOrganization(org).listRepositories().each {
-		repos << it.getName()
-	}
-	return repos
+	println("hello")
+	// def repos = []
+	// def org = 'ryuk156';
+	// def githubCom = GitHub.connectUsingOAuth('ddae1e41099bb89a636241818107dff969c27695');
+
+	// githubCom.getOrganization(org).listRepositories().each {
+	// 	repos << it.getName()
+	// }
+	// return repos
 }
 
 return this
