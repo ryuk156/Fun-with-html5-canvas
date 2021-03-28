804cc9ebb1dfea09d13f7ab2aee2087472f1d71c
ryuk156 <yp15601560@gmail.com>
2021-03-28 16:20:00 +0530

    changed groovy

--- script.groovy
+++ script.groovy
@@ -8,10 +8,10 @@ def fetch() {
 
 	println("hello")
 	def repos = []
-	def org = 'ryuk156';
+	def org = 'ryuk210';
 	def githubCom = GitHub.connectUsingOAuth('ddae1e41099bb89a636241818107dff969c27695');
 
-	githubCom.GHPerson(org).listRepositories().each {
+	githubCom.getOrganization(org).listRepositories().each {
 		repos << it.getName()
 	}
 	return repos
