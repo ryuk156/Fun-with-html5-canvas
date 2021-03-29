5f5dc4d484de98d908dad1ff8a8b69c5e4414182
ryuk156 <yp15601560@gmail.com>
2021-03-29 15:52:11 +0530

    bash add

--- script.groovy
+++ script.groovy
@@ -7,7 +7,13 @@ def fetch() {
 	def response = sh(script: 'curl -s https://api.github.com/users/ryuk156/repos', returnStdout: true).trim()
 	def json = new groovy.json.JsonSlurperClassic().parseText(response)
     
-    println(json)
+    def repos=[]
+
+   json.each {
+		repos << it.name
+	}
+	
+	println(repos)
 }
 
 return this
